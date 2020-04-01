68334581e38727f70abc03b2a7c069cfa8f492dc
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-01 17:09:27 +0000

    Automatically backed up by Learn

--- db/migrate/20200401164424_create_students.rb
+++ db/migrate/20200401164424_create_students.rb
@@ -3,8 +3,6 @@ class CreateStudents < ActiveRecord::Migration[5.0]
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-
-      t.timestamps
     end
   end
 end
